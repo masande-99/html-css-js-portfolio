a3f611fbde7ee7f5864adb1b39e41e6272f7f457
masande <gontyelenimasande@gmail.com>
2021-02-17 15:25:42 +0200

    made a few changes

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function displayDate(){
     document.getElementById("showDate").innerHTML = Date();
+
 }
+
+
+var blink_speed = 1000; // every 1000 == 1 second, adjust to suit
+var t = setInterval(function () {
+    var ele = document.getElementById('blinky');
+    ele.style.visibility = (ele.style.visibility == 'hidden' ? '' : 'hidden');
+}, blink_speed);
